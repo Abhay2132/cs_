27f650923e5e892b40d79f65e1ea3d60b584772c
abhay2132 <abhaybro768@gmail.com>
2023-11-23 14:01:00 +0530

    done

--- abhay/q16.java
+++ abhay/q16.java
@@ -4,7 +4,18 @@ public class q16 {
     public static void main(String[] args) {
         q1.print("Enter a number : ");
         int num = q1.sc.nextInt();
-        int d_count = 0;
-        for(int i=)
+        q1.print("The number which has exactly 9 divisors upto "+num+" are \n");
+        for(int a = 36; a <= num; a++) {
+        	
+        int d_count = 2;
+        for(int i=2; i<=a/2;i++){
+        	if(a%i == 0) d_count += 1;
+       }
+       
+       if(d_count == 9) q1.print(a+" ");
+       
+       }
+       
+      q1.print("\n");
     }    
 }
